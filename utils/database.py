abbf7ba287eac53ea45e7a2b03bb20bba6717ea5
Arjun Pawar <arjunsinghpawar855@gmail.com>
2025-08-04 01:27:59 +0530

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -1,10 +1,11 @@
 import re
 import logging
+from typing import Optional, Tuple, List
 
-from pymongo.errors import DuplicateKeyError
 from beanie import Document, init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
-from marshmallow.exceptions import ValidationError
+from pymongo.errors import DuplicateKeyError
+from pydantic import BaseModel
 
 from info import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER
 from .helpers import unpack_new_file_id
@@ -12,91 +13,89 @@ from .helpers import unpack_new_file_id
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+# Global MongoDB client
 client = AsyncIOMotorClient(DATABASE_URI)
-database = client[DATABASE_NAME]
-instance = Instance.from_db(database)
 
 
-@instance.register
+# Pydantic-style Beanie document
 class Media(Document):
-    file_id = int(attribute='_id')
-    file_ref = str(allow_none=True)
-    file_name = str(required=True)
-    file_size = int(required=True)
-    file_type = str(allow_none=True)
-    mime_type = str(allow_none=True)
-    caption = str(allow_none=True)
+    file_id: str
+    file_ref: Optional[str] = None
+    file_name: str
+    file_size: int
+    file_type: Optional[str] = None
+    mime_type: Optional[str] = None
+    caption: Optional[str] = None
 
-    class Meta:
-        indexes = ('$file_name', )
-        collection_name = COLLECTION_NAME
+    class Settings:
+        name = COLLECTION_NAME
+        indexes = ["file_name"]
 
 
+# Call this once at app startup
+async def init_db():
+    await init_beanie(database=client[DATABASE_NAME], document_models=[Media])
+
+
+# Save media to MongoDB
 async def save_file(media):
-    """Save file in database"""
-
     file_id, file_ref = unpack_new_file_id(media.file_id)
 
+    file = Media(
+        file_id=file_id,
+        file_ref=file_ref,
+        file_name=media.file_name,
+        file_size=media.file_size,
+        file_type=media.file_type,
+        mime_type=media.mime_type,
+        caption=media.caption.html if media.caption else None,
+    )
+
     try:
-        file = Media(
-            file_id=file_id,
-            file_ref=file_ref,
-            file_name=media.file_name,
-            file_size=media.file_size,
-            file_type=media.file_type,
-            mime_type=media.mime_type,
-            caption=media.caption.html if media.caption else None,
-        )
-    except ValidationError:
-        logger.exception('Error occurred while saving file in database')
-    else:
-        try:
-           await file.insert()
-        except DuplicateKeyError:
-            logger.warning(media.file_name + " is already saved in database")
-        else:
-            logger.info(media.file_name + " is saved in database")
+        await file.insert()
+        logger.info(f"{media.file_name} is saved in the database.")
+    except DuplicateKeyError:
+        logger.warning(f"{media.file_name} is already saved in the database.")
 
 
-async def get_search_results(query, file_type=None, max_results=10, offset=0):
-    """For given query return (results, next_offset)"""
-
+# Search and paginate results
+async def get_search_results(query: str, file_type: Optional[str] = None, max_results: int = 10, offset: int = 0) -> Tuple[List[Media], str]:
     query = query.strip()
     if not query:
-        raw_pattern = '.'
-    elif ' ' not in query:
-        raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
+        raw_pattern = "."
+    elif " " not in query:
+        raw_pattern = r"(\b|[\.\+\-_])" + re.escape(query) + r"(\b|[\.\+\-_])"
     else:
-        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_\(\)\[\]]')
+        raw_pattern = re.escape(query).replace(r"\ ", r".*[\s\.\+\-_\(\)\[\]]")
 
     try:
-        regex = re.compile(raw_pattern, flags=re.IGNORECASE)
-    except:
-        return [], ''
+        regex = re.compile(raw_pattern, re.IGNORECASE)
+    except re.error:
+        return [], ""
 
+    filters = {}
     if USE_CAPTION_FILTER:
-        filter = {'$or': [{'file_name': regex}, {'caption': regex}]}
+        filters = {
+            "$or": [
+                {"file_name": {"$regex": regex}},
+                {"caption": {"$regex": regex}},
+            ]
+        }
     else:
-        filter = {'file_name': regex}
+        filters = {"file_name": {"$regex": regex}}
 
     if file_type:
-        filter['file_type'] = file_type
+        filters["file_type"] = file_type
 
-    total_results = await Media.count_documents(filter)
-    next_offset = offset + max_results
+    total_results = await Media.find(filters).count()
+    next_offset = offset + max_results if offset + max_results < total_results else ""
 
-    if next_offset > total_results:
-        next_offset = ''
+    files = (
+        await Media.find(filters)
+        .sort("-_id")  # newest first
+        .skip(offset)
+        .limit(max_results)
+        .to_list()
+    )
 
-    cursor = Media.find(filter)
-
-    # Sort by recent
-    cursor.sort('$natural', -1)
-
-    # Slice files according to offset and max results
-    cursor.skip(offset).limit(max_results)
-
-    # Get list of files
-    files = await cursor.to_list(length=max_results)
-
-    return files, next_offset
+    return files, str(next_offset)
