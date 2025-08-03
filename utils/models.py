5878af282e0e2abde0c36645c59013a4e2f5c537
Arjun Pawar <arjunsinghpawar855@gmail.com>
2025-08-04 03:18:28 +0530

    Update models.py

--- utils/models.py
+++ utils/models.py
@@ -1,22 +1,25 @@
 from beanie import Document, init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
-from pydantic import BaseModel, Field
+from pydantic import Field
 from typing import Optional
 import datetime
 
+
 class Media(Document):
     file_id: str
-    file_name: Optional[str]
-    file_size: Optional[int]
-    file_type: Optional[str]
-    mime_type: Optional[str]
-    caption: Optional[str]
+    file_name: Optional[str] = None
+    file_size: Optional[int] = None
+    file_type: Optional[str] = None
+    mime_type: Optional[str] = None
+    caption: Optional[str] = None
     added_at: datetime.datetime = Field(default_factory=datetime.datetime.utcnow)
 
     class Settings:
-        name = "Telegram_files"
+        name = "Telegram_files"  # collection name
+
+    class Config:
+        arbitrary_types_allowed = True  # in case non-JSON types are added in future
 
     @classmethod
     async def ensure_indexes(cls):
-        await cls.create_index("file_name")
-        await cls.create_index("file_id")
+        await cls.creat
