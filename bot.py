451072501e0bd8020d6b12459a5f76c8447b3cc7
Arjun Pawar <arjunsinghpawar855@gmail.com>
2025-08-04 00:14:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ logging.getLogger().setLevel(logging.WARNING)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from utils import Media
+from core.utils import Media
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN
 
 
