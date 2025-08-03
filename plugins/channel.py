36a3dd3c96229949ad5b0148ad29f51141137bf7
Arjun Pawar <arjunsinghpawar855@gmail.com>
2025-08-04 03:06:11 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,21 +1,35 @@
 from pyrogram import Client, filters
+from pyrogram.types import Message, Document, Audio, Video
 
 from info import CHANNELS
-from utils import save_file
+from utils import save_file  # Your Beanie-based file save function
 
+# Match only documents, videos, or audios
 media_filter = filters.document | filters.video | filters.audio
 
 
 @Client.on_message(filters.chat(CHANNELS) & media_filter)
-async def media(bot, message):
-    """Media Handler"""
-    for file_type in ("document", "video", "audio"):
-        media = getattr(message, file_type, None)
-        if media is not None:
-            break
-    else:
-        return
+async def media_handler(client: Client, message: Message):
+    """Handle media files from monitored channels"""
 
+    media = None
+    file_type = None
+
+    if message.document:
+        media = message.document
+        file_type = "document"
+    elif message.video:
+        media = message.video
+        file_type = "video"
+    elif message.audio:
+        media = message.audio
+        file_type = "audio"
+
+    if not media:
+        return  # Shouldn't occur due to filter, but safe check
+
+    # Inject required attributes before saving
     media.file_type = file_type
     media.caption = message.caption
+
     await save_file(media)
