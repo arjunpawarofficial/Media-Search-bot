84952b423f4ccbe389e95ff679b83360f271c2d7
Arjun Pawar <arjunsinghpawar855@gmail.com>
2025-08-03 23:54:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -6,9 +6,12 @@ id_pattern = re.compile(r'^.\d+$')
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
 USER_SESSION = environ.get('USER_SESSION', 'User_Bot')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+API_ID = int(environ.get('API_ID', '27510642'))
+API_HASH = environ.get('API_HASH', 'fe40f9eba4fd6d1018cc473ef2e1aae1')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7528458385:AAHmbU_m9L6pZpxy-F8dKJzu4sfnxDQDSXI')
+#API_ID = int(environ['API_ID'])
+#API_HASH = environ['API_HASH']
+#BOT_TOKEN = environ['BOT_TOKEN']
 USERBOT_STRING_SESSION = environ.get('USERBOT_STRING_SESSION')
 
 # Bot settings
@@ -16,17 +19,24 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1897434785').split()]
+
+
+#ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS', "https://t.me/enter10movieshubofficial"].split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', "https://t.me/arjunsinghpawar").split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
 
 # MongoDB information
-DATABASE_URI = environ['DATABASE_URI']
-DATABASE_NAME = environ['DATABASE_NAME']
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://arjunsinghpawar855:arjunsinghpawar855@movieshub.jw0ol0o.mongodb.net/?retryWrites=true&w=majority&appName=MoviesHub")
+DATABASE_NAME = environ.get('DATABASE_NAME', "MoviesHub")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+
+#DATABASE_URI = environ['DATABASE_URI']
+#DATABASE_NAME = environ['DATABASE_NAME']
+#COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
 default_start_msg = """
